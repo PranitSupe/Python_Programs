076883a9ee932bee41e0db5a2159365a423fbd8c
Pranit Supe <pranitsupe@gmail.com>
2026-05-08 20:56:24 +0530

    Added basic Python progam

---
+++ Python_Programs/Basics/variables/variable4.py
@@ -0,0 +1,15 @@
+price = 99.99
+age =45
+name="rahul"
+charecter='a'
+
+
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(price)
+print(type(price))
+print(charecter)
+print(type(charecter))
