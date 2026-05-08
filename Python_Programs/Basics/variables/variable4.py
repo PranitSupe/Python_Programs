9df1c7098120bc07aec2cc96b33333d10ca74d78
Pranit Supe <pranitsupe@gmail.com>
2026-05-08 21:01:10 +0530

    Added basic Python progam

--- Python_Programs/Basics/variables/variable4.py
+++ Python_Programs/Basics/variables/variable4.py
@@ -2,9 +2,12 @@ price = 99.99
 age =45
 name="rahul"
 charecter='a'
+is_student = True
 
 
 
+print(is_student)
+print(type(is_student))
 print(name)
 print(type(name))
 print(age)
---
+++ Python_Programs/Basics/variables/variable5.py
@@ -0,0 +1,15 @@
+#Multiple Variables in One Line
+
+x, y, z = 10, 20, 30
+
+print(x)
+print(y)
+print(z)
+
+#Same Value to Multiple Variables
+
+a = b = c = 100
+
+print(a)
+print(b)
+print(c)
