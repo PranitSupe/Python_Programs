668639aae20749ce8b6cf9a5b9b32b24ac085965
Pranit Supe <pranitsupe@gmail.com>
2026-05-10 22:45:02 +0530

    Added basic Python progam

--- Python_Programs/Basics/variables/datatype2.py
+++ Python_Programs/Basics/variables/datatype2.py
@@ -7,7 +7,6 @@ print(type(b))
 print("-------------------------")
 
 a = 10.8
-
 b = int(a)
 
 print(b)
@@ -15,10 +14,25 @@ print(type(b))
 
 print("-------------------------")
 
+num = 100
+text = str(num)
+
+print(text)
+print(type(text))
 
 print("-------------------------")
 
+text = "50"
+num = int(text)
+
+print(num)
+print(type(num))
+
 print("-------------------------")
 
+my_list = [1, 2, 3]
+
+print(my_list)
+print(type(my_list))
 
 print("-------------------------")
