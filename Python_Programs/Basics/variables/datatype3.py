22dafae64e185cc16b3a92fec1235effb80f3629
Pranit Supe <pranitsupe@gmail.com>
2026-05-11 08:48:05 +0530

    Added basic Python progam

---
+++ Python_Programs/Basics/variables/datatype3.py
@@ -0,0 +1,20 @@
+#complex number
+z= 3 + 4j
+print(z)
+print(type(z))
+print(z.real)
+print(z.imag)
+
+print("-------------------")
+
+#bitwise AND
+c=2 & 3
+print(c)
+
+print("-------------------")
+
+#bitwise OR
+d=2 | 3
+print(d)
+
+print("-------------------")
