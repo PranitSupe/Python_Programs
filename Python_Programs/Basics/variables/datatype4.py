5307f67852327ee0e29961159cfb1fa8c37dce95
Pranit Supe <pranitsupe@gmail.com>
2026-05-11 09:52:25 +0530

    Added basic Python progam string

---
+++ Python_Programs/Basics/variables/datatype4.py
@@ -0,0 +1,13 @@
+#string
+
+print("Forword indexing:")
+name="pranit"
+print(name[3])
+print(name[4])
+print(name[2:6:1])
+print("Reverse indexing:")
+text="apple is red"
+print(text[-5])
+print(text[-9])
+print(text[-2:-4:-1])
+print(text[-4::-1])
