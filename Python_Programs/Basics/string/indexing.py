ee74ad25c73fb123a30b3e71b06b0b6faee75cc7
Pranit Supe <pranitsupe@gmail.com>
2026-05-12 09:43:29 +0530

    Added basic Python progam string

---
+++ Python_Programs/Basics/string/indexing.py
@@ -0,0 +1,10 @@
+text="Python is fun"
+print(text[-1::-1])
+print(text[0::2])
+print(text[0::3])
+print(text[9:12:1])
+
+# print "Python" reverse
+print(text[-8::-1])
+print(text[-8:-14:-1])
+print(text[5::-1])
