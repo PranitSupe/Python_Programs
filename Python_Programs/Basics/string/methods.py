3b728385a2a65e22470b1307d347056ff1f10c50
Pranit Supe <pranitsupe@gmail.com>
2026-05-12 10:36:36 +0530

    Added basic Python progam string

--- Python_Programs/Basics/string/methods.py
+++ Python_Programs/Basics/string/methods.py
@@ -6,7 +6,29 @@ print(s.lower())
 s1=s.lower()
 print(s1)
 
-#upper
+print("-----------------------")
+
+#upper()
+r="hello"
+
+print("heLLo".upper())
+print(r.upper())
+r1=r.upper()
+print(r1)
+
+print("-----------------------")
+
+#capitalize()
+print("python is good.today is python class")
+
+print("python is good.today is python class".capitalize())
+
+
+
+
+
+
+
 
 
 
