b72e57854a806a343003f806258a63051b1962bd
Pranit Supe <pranitsupe@gmail.com>
2026-05-13 09:26:07 +0530

    Added basic Python progam string

--- Python_Programs/Basics/string/methods2.py
+++ Python_Programs/Basics/string/methods2.py
@@ -1,15 +1,19 @@
 #replace
 text="banana"
-print("orignal:",text)
-print("after replace:",text.replace("a","o"))
+print("orignal: ",text)
+print("after replace: ",text.replace("a","o"))
 print("------------------------------")
 
 #split
 sentence="i love python"
-print("orignal:",sentence)
-print("after split:",sentence.split(" "))
+print("orignal: ",sentence)
+print("after split: ",sentence.split(" "))
 
 print("------------------------------")
+#join 
+words=['i','love','python']
+print("orignal: ",words)
+print("after join: "," ".join(words))
 
 print("------------------------------")
 
