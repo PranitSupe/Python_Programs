429da6211ecf46397fb1adff9cb9ffc8277e50e8
Pranit Supe <pranitsupe@gmail.com>
2026-05-13 09:14:20 +0530

    Added basic Python progam string

---
+++ Python_Programs/Basics/string/methods2.py
@@ -0,0 +1,20 @@
+#replace
+text="banana"
+print("orignal:",text)
+print("after replace:",text.replace("a","o"))
+print("------------------------------")
+
+#split
+sentence="i love python"
+print("orignal:",sentence)
+print("after split:",sentence.split(" "))
+
+print("------------------------------")
+
+print("------------------------------")
+
+print("------------------------------")
+
+print("------------------------------")
+
+print("------------------------------")
