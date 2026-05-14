087ced8b763665de00722cb63351604d8fed9181
Pranit Supe <pranitsupe@gmail.com>
2026-05-14 12:28:12 +0530

    Added basic Python progam list

--- Python_Programs/Basics/list/list2.py
+++ Python_Programs/Basics/list/list2.py
@@ -1,11 +1,35 @@
 num=[2,3,4,5,6,7,8,9,10]
-print(num)
+print("orignal list: ",num)
+
 
 num.remove(10)
-print(num)
+print("list after remove(10):",num)
 
 num.pop()
-print(num)
+print("list after pop():",num)
+
 
 del num[0]
-print(num)
+print("list after deleting index[0]:",num)
+
+
+
+print("index of element(6):",num.index(6))
+
+num.sort()
+print("list after sorting(asc):",num)
+
+num.sort(reverse=True)
+print("list after sorting(desc):",num)
+
+
+print("length of list:",len(num))
+
+total=sum(num)
+print("sum of list items:",total)
+
+
+c=['d1','d2','d3']
+print(" list before clear:",c)
+c.clear()
+print(" list after clear:",c)
