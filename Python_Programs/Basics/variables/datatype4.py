012cfb500d8a9486f704402852d13d6649a71687
Pranit Supe <pranitsupe@gmail.com>
2026-05-15 10:05:29 +0530

    Added basic Python progam variables

--- Python_Programs/Basics/variables/datatype4.py
+++ Python_Programs/Basics/variables/datatype4.py
@@ -4,7 +4,9 @@ print("Forword indexing:")
 name="pranit"
 print(name[3])
 print(name[4])
-print(name[2:6:1])
+print(name[2:5:1])
+print(name[2::1])
+
 print("Reverse indexing:")
 text="apple is red"
 print(text[-5])
