f7fbc1262b55af8680c2bcb66432a659b1039b93
Pranit Supe <pranitsupe@gmail.com>
2026-05-16 17:54:10 +0530

    Added basic Python progam data type

---
+++ Python_Programs/Basics/variables/variable8.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5.5
+c = "Python"
+d = True
+e = [1, 2, 3]
+f = (1, 2, 3)
+g = {1, 2, 3}
+h = {"name": "Pranit"}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
