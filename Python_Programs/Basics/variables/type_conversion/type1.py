f2b3e0e82ae0b19cd5bcf493cea9a42a29b8b4d6
Pranit Supe <pranitsupe@gmail.com>
2026-05-16 17:36:07 +0530

    Added basic Python progam type conversion

--- Python_Programs/Basics/variables/type_conversion/type1.py
+++ Python_Programs/Basics/variables/type_conversion/type1.py
@@ -8,3 +8,35 @@ print(type(result))
 
 
 #2
+num = 10.9
+
+result = int(num)
+
+print(result)
+print(type(result))
+
+#3
+num = 100
+
+result = str(num)
+
+print(result)
+print(type(result))
+
+#4
+text = "50"
+
+result = int(text)
+
+print(result)
+print(type(result))
+
+#5
+text = "25.5"
+
+result = float(text)
+
+print(result)
+print(type(result))
+
+
