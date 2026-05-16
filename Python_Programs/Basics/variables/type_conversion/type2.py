79c63c43f1631da8bc1e061f9c44b26f03ed498f
Pranit Supe <pranitsupe@gmail.com>
2026-05-16 17:52:11 +0530

    Added basic Python progam type conversion

---
+++ Python_Programs/Basics/variables/type_conversion/type2.py
@@ -0,0 +1,39 @@
+#6
+my_list = [1, 2, 3]
+
+result = tuple(my_list)
+
+print(result)
+print(type(result))
+
+#7
+my_tuple = (1, 2, 3)
+
+result = list(my_tuple)
+
+print(result)
+print(type(result))
+
+#8
+my_list = [1, 2, 2, 3]
+
+result = set(my_list)
+
+print(result)
+print(type(result))
+
+#9
+my_set = {1, 2, 3}
+
+result = list(my_set)
+
+print(result)
+print(type(result))
+
+#10
+value = True
+
+result = int(value)
+
+print(result)
+print(type(result))
