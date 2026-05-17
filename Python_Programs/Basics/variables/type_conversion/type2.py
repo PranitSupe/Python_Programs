4b5628ba5ac51e1d623ac5710e17c3a5cc986cc4
Pranit Supe <pranitsupe@gmail.com>
2026-05-17 23:14:55 +0530

    Added basic Python progam data type

--- Python_Programs/Basics/variables/type_conversion/type2.py
+++ Python_Programs/Basics/variables/type_conversion/type2.py
@@ -1,39 +1,45 @@
-#6
+#6 Convert list to tuple.
+
 my_list = [1, 2, 3]
-
 result = tuple(my_list)
-
 print(result)
 print(type(result))
 
-#7
+print("--------------------------")
+
+#7 Convert tuple to list.
+
 my_tuple = (1, 2, 3)
-
 result = list(my_tuple)
-
 print(result)
 print(type(result))
 
-#8
+print("--------------------------")
+
+#8 Convert list to set.
+
 my_list = [1, 2, 2, 3]
-
 result = set(my_list)
-
 print(result)
 print(type(result))
 
-#9
+print("--------------------------")
+
+#9 Convert set to list
+
 my_set = {1, 2, 3}
-
 result = list(my_set)
-
 print(result)
 print(type(result))
 
-#10
+print("--------------------------")
+
+#10 Convert boolean to integer.
+
 value = True
-
-result = int(value)
-
+result= int(value)
 print(result)
 print(type(result))
+
+
+print("--------------------------")
--- Python_Programs/Basics/variables/type_conversion/type3.py
+++ Python_Programs/Basics/variables/type_conversion/type3.py
@@ -6,12 +6,16 @@ print(result)
 print(type(result))
 
 
+print("--------------------------")
+
 #12 Take number as input and convert it to float.
 
 num=input("enter a number:")
 result=float(num)
 print(result)
 print(type(result))
+
+print("--------------------------")
 
 #13 Take decimal input and convert to integer.
 
@@ -20,7 +24,19 @@ result= int(float(num))
 print(result)
 print(type(result))
 
+print("--------------------------")
+
 
 #14 Convert character into ASCII value.
+
 char='B'
 print(ord(char))
+
+print("--------------------------")
+
+#15 Convert ASCII value into character.
+
+num=65
+print(chr(num))
+
+print("--------------------------")
