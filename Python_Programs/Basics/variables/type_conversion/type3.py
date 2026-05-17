b85f9bc352f0662e646691380984a1df805480a2
Pranit Supe <pranitsupe@gmail.com>
2026-05-17 21:15:17 +0530

    Added basic Python progam data type

--- Python_Programs/Basics/variables/type_conversion/type3.py
+++ Python_Programs/Basics/variables/type_conversion/type3.py
@@ -1,4 +1,4 @@
-#11
+#11 Convert integer to boolean.
 
 num=1
 result= bool(num)
@@ -6,8 +6,21 @@ print(result)
 print(type(result))
 
 
+#12 Take number as input and convert it to float.
 
 num=input("enter a number:")
 result=float(num)
 print(result)
 print(type(result))
+
+#13 Take decimal input and convert to integer.
+
+num=input("enter decimal num:")
+result= int(float(num))
+print(result)
+print(type(result))
+
+
+#14 Convert character into ASCII value.
+char='B'
+print(ord(char))
