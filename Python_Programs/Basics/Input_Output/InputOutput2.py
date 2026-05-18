fd090ed5b54b6e4749cae97ac97794759e5955e6
Pranit Supe <pranitsupe@gmail.com>
2026-05-18 20:16:53 +0530

    Added basic Python progam InputOutput

---
+++ Python_Programs/Basics/Input_Output/InputOutput2.py
@@ -0,0 +1,25 @@
+#6 Take two numbers and print division result.
+
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+
+print("division:",a/b)
+
+print("-------------------")
+#7 Take student details and print them.
+
+name=input("enter name:")
+age=int(input("enter age:"))
+city=input("enter city:")
+
+print("name:",name)
+print("age:",age)
+print("city:",city)
+
+print("-------------------")
+#8 Print multiple values in one line.
+print("-------------------")
+#9 Print output using f-string.
+print("-------------------")
+#10 Print numbers from 1 to 5 in one line.
+print("-------------------")
