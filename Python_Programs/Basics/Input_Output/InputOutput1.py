b5b5cb0e23285d266bd4dfe0528b909b6a0da97b
Pranit Supe <pranitsupe@gmail.com>
2026-05-18 19:43:38 +0530

    Added basic Python progam InputOutput

---
+++ Python_Programs/Basics/Input_Output/InputOutput1.py
@@ -0,0 +1,22 @@
+#1 Take user name as input and print it.
+
+name=input("enter your name:")
+print(name)
+
+print("----------------------")
+
+#2 Take age as input and print:
+
+age=input("enter your age:")
+print(f"your age is {age}")
+
+print("----------------------")
+#3
+
+
+print("----------------------")
+#4
+
+
+print("----------------------")
+#5
