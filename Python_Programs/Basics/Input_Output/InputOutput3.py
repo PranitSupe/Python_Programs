bab50d80b501e8560cfe5a9ce3d704de8fff2958
Pranit Supe <pranitsupe@gmail.com>
2026-05-19 23:04:59 +0530

    Added basic Python progam InputOutput

--- Python_Programs/Basics/Input_Output/InputOutput3.py
+++ Python_Programs/Basics/Input_Output/InputOutput3.py
@@ -23,5 +23,15 @@ print("hello\nworld")
 print("hello\tworld")
 print("--------------------")
 
-#15
+#15 Create a mini calculator using input.
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+
+print("addition:",num1+num2)
+
+print("substraction:",num1-num2)
+
+print("multiplication:",num1*num2)
+
+print("division:",num1/num2)
 print("--------------------")
