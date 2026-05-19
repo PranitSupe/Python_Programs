9279237c68b64486eb5b5886bc1f12df955227fe
Pranit Supe <pranitsupe@gmail.com>
2026-05-19 22:55:17 +0530

    Added basic Python progam InputOutput

---
+++ Python_Programs/Basics/Input_Output/InputOutput3.py
@@ -0,0 +1,19 @@
+#11 Take float input and print it.
+
+num=float(input("enter a float number:"))
+print("float number:",num)
+print("--------------------")
+
+#12 Take boolean input and print it.
+
+num=bool(input("enter a boolean value:"))
+print("boolean value:",num)
+print("--------------------")
+
+#13 Print formatted output.
+
+print("--------------------")
+#14
+print("--------------------")
+#15
+print("--------------------")
