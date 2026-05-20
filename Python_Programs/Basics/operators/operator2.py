49974bf83a0e1c6e2ce74fb9daf7f78371ba340d
Pranit Supe <pranitsupe@gmail.com>
2026-05-20 20:06:55 +0530

    Added basic python program datatype

---
+++ Python_Programs/Basics/operators/operator2.py
@@ -0,0 +1,36 @@
+##  Assignment Operators
+
+
+#8  Add and Assign
+a=10
+b=3
+a+=b
+print(" Added and Assigned result:",a)
+print("---------------------")
+
+#8 Subtract and Assign
+
+a=10
+b=3
+a-=b
+print(" Subtracted and Assigned result:",a)
+
+print("---------------------")
+
+#8 Multiply and Assign
+
+a=10
+b=3
+a*=b
+print(" Multiplied and Assigned result:",a)
+
+print("---------------------")
+
+#8 Divide and Assign
+
+a=10
+b=3
+a/=b
+print(" Divided and Assigned result:",a)
+
+print("---------------------")
