764b9ea776785916c2d2aac805c0f95ddbb803e3
Pranit Supe <pranitsupe@gmail.com>
2026-05-20 20:08:28 +0530

    Added basic python program operator

--- Python_Programs/Basics/operators/operator2.py
+++ Python_Programs/Basics/operators/operator2.py
@@ -6,7 +6,7 @@ a=10
 b=3
 a+=b
 print(" Added and Assigned result:",a)
-print("---------------------")
+print("-----------------------")
 
 #8 Subtract and Assign
 
@@ -15,7 +15,7 @@ b=3
 a-=b
 print(" Subtracted and Assigned result:",a)
 
-print("---------------------")
+print("-----------------------")
 
 #8 Multiply and Assign
 
@@ -24,7 +24,7 @@ b=3
 a*=b
 print(" Multiplied and Assigned result:",a)
 
-print("---------------------")
+print("------------------------")
 
 #8 Divide and Assign
 
@@ -33,4 +33,4 @@ b=3
 a/=b
 print(" Divided and Assigned result:",a)
 
-print("---------------------")
+print("------------------------")
