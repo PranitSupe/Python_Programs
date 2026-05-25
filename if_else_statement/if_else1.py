3cf4843f9ee6cbeae21c45a12c8f943e39855fa5
Pranit Supe <pranitsupe@gmail.com>
2026-05-25 19:56:39 +0530

    added python program if_else statement

--- if_else_statement/if_else1.py
+++ if_else_statement/if_else1.py
@@ -73,3 +73,12 @@ else:
     print("both strings are different")
 
 
+#9 Check Leap Year
+year=int(input("enter a year:"))
+if (year%4==0 and year%100!=0) or (year%400==0):
+    print(year,"is a leap year")
+else:
+    print(year,"is not a leap year")
+
+#10 Check Number is Zero or Not:
+num=int(input("enter a number:"))
