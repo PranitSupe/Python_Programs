3986dfb204fb6aa953b2603f7afd00c7dbf6e8a6
Pranit Supe <pranitsupe@gmail.com>
2026-05-26 21:09:27 +0530

    added python program if_elif_else statement

--- if_else_statement/if_elif_else1.py
+++ if_else_statement/if_elif_else1.py
@@ -100,3 +100,18 @@ else:
     print("invalid operator")
       
 print("----------------------")
+
+#7 Check Age Category
+
+age=int(input("enter age:"))
+
+if age<13:
+    print("child")
+elif (age>=13 and age<20):
+    print("teenager")
+elif (age>=20 and age<60):
+    print("adult")
+else:
+    print("senior citizen")
+
+print("----------------------")
