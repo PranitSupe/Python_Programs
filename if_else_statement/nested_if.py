106eb2960dc940492b62d2cc915e7102fa335d5e
Pranit Supe <pranitsupe@gmail.com>
2026-05-27 22:37:35 +0530

    added python program nested_if statement

--- if_else_statement/nested_if.py
+++ if_else_statement/nested_if.py
@@ -6,8 +6,10 @@ marks=int(input("enter marks:"))
 income=int(input("enter income:"))
 
 if marks>=80:
-    if income<50000:
+    if income<=50000:
         print("eligible for scholarship")
+    else:
+        print("not eligible for scholarship")
 else:
     print("not eligible for scholarship")
 
@@ -20,4 +22,19 @@ password=input("enter password:")
 if uid=="UserName":
     if password=="Pass@123":
         print("login Successful")
-print("Login Failed")
+    else:
+        print("Login Failed")
+else:
+    print("Login Failed")
+
+#3 Positive Even Number:
+
+num=int(input("enter a number:"))
+
+if num>0:
+    if num%2==0:
+        print(num,"is positive even number")
+    else:
+        print(num,"is positive number but not even")
+else:
+    print(num,"is neither even nor positive")
