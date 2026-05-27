c2767589e986028c4e126f328470f24e2c532cd4
Pranit Supe <pranitsupe@gmail.com>
2026-05-27 22:12:47 +0530

    added python program nested_if statement

--- if_else_statement/nested_if.py
+++ if_else_statement/nested_if.py
@@ -10,3 +10,14 @@ if marks>=80:
         print("eligible for scholarship")
 else:
     print("not eligible for scholarship")
+
+
+#2 Login System
+
+uid=input("enter user id:")
+password=input("enter password:")
+
+if uid=="UserName":
+    if password=="Pass@123":
+        print("login Successful")
+print("Login Failed")
