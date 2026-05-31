5035407795ae28c760846a0497b2e9b8f7b6efa3
Pranit Supe <pranitsupe@gmail.com>
2026-05-31 22:46:46 +0530

    added python program shortHand_if statement

---
+++ if_else_statement/short_if.py
@@ -0,0 +1,13 @@
+## shorthand if_statement:
+
+#1 voting eligiblity:
+
+age =int(input("enter your age:"))
+
+if age >= 18: print("Eligible for Voting")
+
+#2 Check Positive Number
+
+num=int(input("enter a number:"))
+
+if num>=0: print("positive number")
