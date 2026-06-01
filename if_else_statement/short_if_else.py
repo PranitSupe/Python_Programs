1396e21a5922ee064f814c36a51c8ed749567c6b
Pranit Supe <pranitsupe@gmail.com>
2026-06-01 22:51:16 +0530

    added python program shortHand_if_else statement

--- if_else_statement/short_if_else.py
+++ if_else_statement/short_if_else.py
@@ -9,3 +9,15 @@ print("even number") if num%2==0 else print("odd number")
 
 num=int(input("enter a number:"))
 print("positive number") if num>0 else print("negative number")
+
+#3 Eligible for Voting
+age=int(input("enter your age:"))
+print("eligible for voting") if age>=18 else print("not eligible for voting")
+
+
+#4 Largest of Two Numbers
+n1=int(input("enter num1:"))
+n2=int(input("enter num2:"))
+
+print("num1 is greater ") if n1>n2 else print("num2 is greater")
+
