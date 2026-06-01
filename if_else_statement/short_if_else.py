f9ddd3e04c1ec39d50ff58492ca5621c0f16db2b
Pranit Supe <pranitsupe@gmail.com>
2026-06-01 23:08:30 +0530

    added python program shortHand_if_else statement

--- if_else_statement/short_if_else.py
+++ if_else_statement/short_if_else.py
@@ -42,3 +42,5 @@ print("Equal") if a == b else print("Not Equal")
 
 num = int(input("enter a number:"))
 print("divisible by 10") if num%10==0 else print("not divisible by 10")
+
+print("---------------------------------")
