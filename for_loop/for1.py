9b6bedb1095524c3214e4a3a1a45042947ba42ae
Pranit Supe <pranitsupe@gmail.com>
2026-06-02 20:22:30 +0530

    added python program  for loop

---
+++ for_loop/for1.py
@@ -0,0 +1,21 @@
+## for loop
+
+#1 Print numbers from 1 to 10.
+
+for i in range(1,11):
+    print(i)
+
+#2. Print numbers from 10 to 1.
+ 
+for i in range(10,0,-1):
+    print(i)
+
+
+#3. Print your name 5 times using a loop.
+for i in range(5):
+    print("pranit")
+
+#4. Print all even numbers from 1 to 20.
+
+
+#5. Print all odd numbers from 1 to 20.
