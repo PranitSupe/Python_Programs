f4c11f42e375ca02d5c197c86215223c3f4de22e
Pranit Supe <pranitsupe@gmail.com>
2026-06-04 21:55:32 +0530

    added python program  for loop

--- for_loop/demo.py
+++ for_loop/demo.py
@@ -8,3 +8,13 @@
 
 
 #19. Find the largest number in a list without using max().
+
+list=[1,2,8,4,5,6]
+max=0
+
+for i in list:
+    if i>max:
+        max=i
+    else:
+        max=max
+print("maximum:",max)
