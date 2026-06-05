3d0846d82478c94ad65d43818942da912bdb420b
Pranit Supe <pranitsupe@gmail.com>
2026-06-05 23:07:57 +0530

    added python program  while loop

--- while_loop/while1.py
+++ while_loop/while1.py
@@ -1,7 +1,35 @@
 ## while_loop:
 
 #1. Print numbers from 1 to 10 using a while loop.
+
 num=1
 while(num<=10):
     print(num)
     num=num+1
+
+print("----------------------------")
+
+#2. Print numbers from 10 to 1.
+
+num=10
+while(num>=1):
+    print(num)
+    num=num-1
+
+print("----------------------------")
+
+
+#3. Print all even numbers from 1 to 20.
+
+print("----------------------------")
+
+
+#4. Print all odd numbers from 1 to 20.
+print("----------------------------")
+
+
+#5. Find the sum of numbers from 1 to 100.
+print("----------------------------")
+
+#6. Print your name 5 times.
+print("----------------------------")
