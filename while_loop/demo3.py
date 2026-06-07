1cadd47ef570dc8b45840e174840d6d3aed62f00
Pranit Supe <pranitsupe@gmail.com>
2026-06-07 22:11:18 +0530

    added python program  while loop

--- while_loop/demo3.py
+++ while_loop/demo3.py
@@ -6,11 +6,25 @@ while num > 0:
     last_digit = num % 10
     rev = (rev * 10) + last_digit
     num = num // 10
-print(rev)
+print("reversed number",rev)
 
 print("----------------------------")
 
 #11. Check whether a number is a palindrome.
+
+num=123
+rev = 0
+while num > 0:
+    last_digit = num % 10
+    rev = (rev * 10) + last_digit
+    num = num // 10
+
+if num==rev:
+    print("number is pelindrome")
+else:
+    print
+
+
 
 print("----------------------------")
 
