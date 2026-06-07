4213c8342ae0ea38e3e867f41da6d8353cb465cc
Pranit Supe <pranitsupe@gmail.com>
2026-06-07 22:06:27 +0530

    added python program  while loop

--- while_loop/while1.py
+++ while_loop/while1.py
@@ -64,9 +64,9 @@ print("----------------------------")
 
 #7. Print the multiplication table of a given number.
 
-num=5
+num=int(input("enter a number:"))
 i=1
-while(i>=10):
+while(i<=10):
     print(num,"X",i,"=",(num*i))
     i=i+1
 
@@ -74,12 +74,33 @@ print("----------------------------")
 
 #8. Count the number of digits in a number.
 
+number=input("enter a number:")
+i=1
+while(i<=len(number)-1):
+    i=i+1
+print("number of digit in number:",i)
+
 print("----------------------------")
 
 #9. Find the sum of digits of a number.
 
-print("----------------------------")
+num = 123
+sum = 0
 
+while num > 0:
+    rem = num % 10          # Extract the last digit
+    sum =sum + rem      # Add it to the sum
+    num = num // 10         # Remove the last digit
+print("sum of digit:",sum)
+
+number=input("enter a number:")
+sum=0
+for i in number:
+    sum=sum+int(i)
+print("sum of digit in number:",sum)
+
+
+print("----------------------------")
 #10. Reverse a number.
 
 print("----------------------------")
