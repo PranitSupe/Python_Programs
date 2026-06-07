adb2f2c5d99d427fe3f60d051200f817608bdd93
Pranit Supe <pranitsupe@gmail.com>
2026-06-07 22:04:59 +0530

    added python program  while loop

--- while_loop/demo3.py
+++ while_loop/demo3.py
@@ -38,7 +38,14 @@ print("sum of digit in number:",sum)
 
 print("----------------------------")
 
-#10. Reverse a number.
+#10. Reverse a number. #***
+num=123
+rev = 0
+while num > 0:
+    last_digit = num % 10
+    rev = (rev * 10) + last_digit
+    num = num // 10
+print(rev)
 
 print("----------------------------")
 
