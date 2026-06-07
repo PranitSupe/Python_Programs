21295d67e7e4f4cfa4a586b2c2f2d03a071770e8
Pranit Supe <pranitsupe@gmail.com>
2026-06-07 22:21:40 +0530

    added python program  while loop

--- while_loop/demo3.py
+++ while_loop/demo3.py
@@ -15,14 +15,14 @@ print("----------------------------")
 num=123
 rev = 0
 while num > 0:
-    last_digit = num % 10
-    rev = (rev * 10) + last_digit
-    num = num // 10
+    last_digit = num % 10              #last_digit=3     #last_digit=2    #last_digit=1  
+    rev = (rev * 10) + last_digit      #rev=3            #rev=32          #rev=321
+    num = num // 10                    #num=12           #num=1           #num=0
 
 if num==rev:
     print("number is pelindrome")
 else:
-    print
+    print("number is not pelindrome")
 
 
 
