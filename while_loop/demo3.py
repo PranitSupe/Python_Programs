15509f8e1ab136f947c9fea882fe2e8116c2cf7b
Pranit Supe <pranitsupe@gmail.com>
2026-06-10 21:09:28 +0530

    added python program  while loop

--- while_loop/demo3.py
+++ while_loop/demo3.py
@@ -36,6 +36,7 @@ while (temp > 0):
         max=max
 
 print("maximum number:",max)
+print("----------------------")
 
 #17. Find the product of all digits in a number
 
@@ -51,4 +52,51 @@ while (temp > 0):
    
 
 print("sum of digit:",sum)
+print("----------------------")
 
+#18. Create a menu-driven calculator that continues until the user chooses Exit.
+
+while(1):
+    print("-----------------------")
+    n1=int(input("enter a number 1:"))
+    n2=int(input("enter a number 2:"))
+    print("-------Menu--------------")
+    print("1. Addition")
+    print("2. Substraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("0. Exit")
+    print("-----------------------")
+    
+    print("enter an operation number (0-4):")
+    op=input("")
+
+    if (op=="1"):
+        print("Addition:",n1+n2)
+    elif (op=="2"):
+        print("Substraction:",n1-n2)
+    elif (op=="3"):
+        print("Multiplication:",n1*n2)
+    elif (op=="4"):
+        print("Division:",n1/n2)
+    elif (op=="0"):
+        print("exit...")
+        break
+      
+print("----------------------")
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+#19. Guess the secret number until the correct answer is entered.
+#20. Simulate an ATM menu (Balance Check, Deposit, Withdraw, Exit).
