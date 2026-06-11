220b8f209ee9269805d5781be2a4ab183d803b53
Pranit Supe <pranitsupe@gmail.com>
2026-06-11 19:11:59 +0530

    added python program  OOPs_class

---
+++ oops_python/class.py
@@ -0,0 +1,52 @@
+##Topic 1: Classes
+
+#Basic
+
+#1.Create an empty class named Student.
+
+class Student:
+    pass
+
+#2.Create an empty class named Book.
+ 
+class Book:
+    pass
+
+#3.Create an empty class named Mobile.
+
+class Mobile:
+    pass
+
+#4.Create an empty class named Employee.
+
+class Employee:
+    pass
+
+#5.Print the type of the class Student.
+
+class Student:
+    pass
+
+print("----------------------------------------------")
+
+#Intermediate
+
+#6.Create classes Car, Bike, and Bus in one program.
+#7.Create a class named Hospital and print the class itself.
+#8.Create a class named Bank and check its type using type().
+#9.Create five different empty classes in a single file.
+#10.Create a class named Laptop and store it in another variable, then print both.
+
+
+print("----------------------------------------------")
+
+
+#Advanced
+#11.Create 10 empty classes related to a college management system.
+#12.Create a class with a long descriptive name and print it.
+#13.Create a class and verify that it is an object of type type.
+#14.Create a program containing only class definitions for a library system.
+#15.Design class names for an ATM system without writing any methods or attributes.
+
+
+print("----------------------------------------------")
