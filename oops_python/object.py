c9f5ca151f4b00b145b17070599fc9b2cf2a56aa
Pranit Supe <pranitsupe@gmail.com>
2026-06-12 22:41:46 +0530

    added python program  OOPs_object

--- oops_python/object.py
+++ oops_python/object.py
@@ -10,8 +10,18 @@ s1=Student()
 
 #Q2.Create a class Car and create one object c1.
 
+class Car:
+    pass
+
+c1=Car()
+
 #Q3.Create a class Book and create one object b1.
 
+class Book:
+    pass
+b1=Book()
+
 #Q4.Print the object s1.
+
 
 #Q5.Print the type of object s1.
