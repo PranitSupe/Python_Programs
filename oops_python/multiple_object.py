5cadacbad600fcd0fe12999e16bfa72b19ba8579
Pranit Supe <pranitsupe@gmail.com>
2026-06-14 21:44:39 +0530

    added python program  OOPs_object

--- oops_python/multiple_object.py
+++ oops_python/multiple_object.py
@@ -1,13 +1,52 @@
 ##Basic (1–5)
 #Q1.Create a class Student and create two objects.
 
+class Student:
+    pass
+
+s1=Student()
+s2=Student()
+
+print("----------------------------------")
+
 #Q2.Create a class Car and create three objects.
+
+class Car:
+    pass
+
+c1=Car()
+c2=Car()
+c3=Car()
+
+
+print("----------------------------------")
 
 #Q3.Print all created objects.
 
+print(c1)
+print(c2)
+print(c3)
+
+
+print("----------------------------------")
+
 #Q4.Print IDs of two Student objects.
 
+print(id(c1))
+print(id(c2))
+print(id(c3))
+
+print("----------------------------------")
+
 #Q5.Check whether two objects have the same ID.
+
+if (id(c1)==id(c2)):
+    print("same")
+else:
+    print("not same")
+
+
+print("----------------------------------")
 
 ##Intermediate (6–10)
 #Q6.Create five objects of class Book.
