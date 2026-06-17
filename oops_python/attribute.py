8989add27851a518817a01cb009f102a62590872
Pranit Supe <pranitsupe@gmail.com>
2026-06-17 18:13:05 +0530

    added python program  OOPs_attribute

--- oops_python/attribute.py
+++ oops_python/attribute.py
@@ -52,5 +52,84 @@ b1.title="Rich Dad Poor Dad"
 print("----------------------------------")
 
 
+#Q6.Create a Student object with:
+#name ,age. Print both.
+
+
+class Student:
+    pass
+
+s1=Student()
+
+s1.name="Pranit"
+s1.age=20
+
+print(s1.name)
+print(s1.age)
+
+print("----------------------------------")
+
+
+#Q7.Create a Product object with:
+#name,price
+#Print both.
+
+class Product:
+    pass
+
+p1=Product()
+
+p1.name="Shoes"
+p1.price=800
+
+print(p1.name)
+print(p1.price)
+
+
+
+print("----------------------------------")
+
+
+#Q8.Create an Employee object with:
+#name
+#salary
+#department
+#Print all details.
+
+class Employee:
+    pass
+
+e1=Employee()
+
+e1.name="Rahul"
+e1.salary=40000
+e1.department="Account"
+
+print(e1.name)
+print(e1.salary)
+print(e1.department)
+
+
+
+print("----------------------------------")
+
+
+#Q9.Create two Student objects with different names.
+#Print both.
+
+
+print("----------------------------------")
+
+
+#Q10.Update multiple attributes of an object.
+#Example:
+#s1.name = "Pranit"
+#s1.age = 20
+
+#s1.name = "Rahul"
+#s1.age = 21
+
+
+print("----------------------------------")
 
 
