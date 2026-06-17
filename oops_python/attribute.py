4953610236d13b05415e736d80012f94bfd0b5c6
Pranit Supe <pranitsupe@gmail.com>
2026-06-17 17:53:08 +0530

    added python program  OOPs_attribute

--- oops_python/attribute.py
+++ oops_python/attribute.py
@@ -22,8 +22,35 @@ c1.brand="BMW"
 
 #Q3.Create a Book object and add a title attribute.
 
+class Book:
+    pass
+
+b1=Book()
+
+b1.title="Power of Mind"
+
+
+print("----------------------------------")
+
 
 #Q4.Print an attribute value.
 
+print(s1.name)
+print(c1.brand)
+print(b1.title)
+
+print("----------------------------------")
 
 #Q5.Modify an existing attribute value.
+
+
+s1.name="Rahul"
+c1.brand="Mahindra"
+b1.title="Rich Dad Poor Dad"
+
+
+print("----------------------------------")
+
+
+
+
