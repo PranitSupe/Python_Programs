992b736fbce53b098da5ab05bb748bb3119a16a4
Pranit Supe <pranitsupe@gmail.com>
2026-06-17 17:45:38 +0530

    added python program  OOPs_attribute

--- oops_python/attribute.py
+++ oops_python/attribute.py
@@ -2,11 +2,20 @@
 
 #Q1.Create a Student object and add a name attribute.
 
+class Student:
+    pass
+
+s1=Student()
+
+s1.name="Pranit"
 
 #Q2.Create a Car object and add a brand attribute.
 
+
 #Q3.Create a Book object and add a title attribute.
 
+
 #Q4.Print an attribute value.
+
 
 #Q5.Modify an existing attribute value.
