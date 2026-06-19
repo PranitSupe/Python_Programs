25b0dfe436146e97b5747505ef9a524172aa867e
Pranit Supe <pranitsupe@gmail.com>
2026-06-19 21:09:47 +0530

    added python program  OOPs_method

---
+++ oops_python/methods.py
@@ -0,0 +1,59 @@
+##methods:
+
+#Q1.Create a class Student with method display() that prints:
+#Hello Student
+
+class Student:
+    def display(self):
+        print("Hello Student")
+
+s1=Student()
+s1.display()
+
+print("------------------------------------------")
+
+
+#Q2.Create a class Car with method start().
+#Output: Car Started
+
+class car:
+    def start(self):
+        print("car started")
+
+c1=car()
+c1.start()
+
+print("------------------------------------------")
+
+#Q3.Create a class Book with method show_title().
+#Print any book title.
+
+class Book:
+    def show_title(self):
+        print("Good Habits")
+
+b1=Book()
+b1.show_title()
+
+print("------------------------------------------")
+
+
+
+#Q4.Create a class Employee with method greet().
+#Print:Welcome Employee
+
+class Employee:
+    def greet(self):
+        print("Welcome Employee")
+
+e1=Employee()
+e1.greet()
+
+print("------------------------------------------")
+
+
+#Q5.Create a class Mobile with method ring().
+#Print:Phone Ringing...
+
+
+print("------------------------------------------")
