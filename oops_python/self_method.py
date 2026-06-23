f696c4c8ddae03ea6f3d9dd06a28ab203b62392f
Pranit Supe <pranitsupe@gmail.com>
2026-06-23 21:39:33 +0530

    added python program  OOPs_self

---
+++ oops_python/self_method.py
@@ -0,0 +1,40 @@
+##SELF
+# Basic (1–5)
+# Q1.Create a method that prints self.
+
+class greet:
+    def to_greet(self):
+        print(self)
+
+g1=greet()
+g1.to_greet()
+    
+print("------------------------------")
+
+# Q2.Create a method that prints id(self).
+
+class greet:
+    def to_greet(self):
+        print(id(self))
+
+g1=greet()
+g1.to_greet()
+
+
+print("------------------------------")
+
+
+# Q3.Create two objects and call the same method.
+
+print("------------------------------")
+
+
+# Q4.Write a program without self and observe the error.
+
+print("------------------------------")
+
+
+# Q5.Print id(s1) outside the class and id(self) inside the method.
+# Compare them.
+
+print("------------------------------")
