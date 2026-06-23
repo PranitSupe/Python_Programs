062b0af0116ee0ed6a00510826011b4687802f68
Pranit Supe <pranitsupe@gmail.com>
2026-06-23 21:26:50 +0530

    added python program  OOPs_self

---
+++ oops_python/self.py
@@ -0,0 +1,22 @@
+Basic (1–5)
+Q1
+
+Create a method that prints self.
+
+Q2
+
+Create a method that prints id(self).
+
+Q3
+
+Create two objects and call the same method.
+
+Q4
+
+Write a program without self and observe the error.
+
+Q5
+
+Print id(s1) outside the class and id(self) inside the method.
+
+Compare them.
