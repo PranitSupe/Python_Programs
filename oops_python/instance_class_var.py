3a8c0e6134c7451f40003e5f8bfab5973f495d7d
Pranit Supe <pranitsupe@gmail.com>
2026-07-06 19:14:37 +0530

    added python program  instance and class var

--- oops_python/instance_class_var.py
+++ oops_python/instance_class_var.py
@@ -1,54 +1,31 @@
-# Q1
-
-# Create a Student class with:
-
+# Q1.Create a Student class with:
 # Instance Variable → name
 # Class Variable → college = "Modern College"
-
 # Print both.
 
-# Q2
-
-# Create an Employee class.
-
+# Q2.Create an Employee class.
 # Instance Variable:
-
 # employee_name
-
 # Class Variable:
-
 # company = "TCS"
-# Q3
 
-# Create a Mobile class.
-
+# Q3.Create a Mobile class.
 # Instance Variable:
-
 # model
-
 # Class Variable:
-
 # brand = "Samsung"
-# Q4
 
-# Create a Car class.
-
+# Q4.Create a Car class.
 # Instance Variable:
-
 # color
-
 # Class Variable:
-
 # wheels = 4
-# Q5
 
-# Create a Book class.
-
+# Q5.Create a Book class.
 # Instance Variable:
-
 # title
-
 # Class Variable:
-
 # language = "English"
+
+
 # Intermediate (6–10)
