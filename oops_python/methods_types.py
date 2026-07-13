3c59b07e42116af91aac6b6fe1b2d9c2492bcefc
Pranit Supe <pranitsupe@gmail.com>
2026-07-13 21:24:52 +0530

    added python program  methods types

---
+++ oops_python/methods_types.py
@@ -0,0 +1,27 @@
+# Basic (1–5)
+# Q1
+
+# Create a Student class with an instance method show_name() that displays a student's name.
+
+# Q2
+
+# Create an Employee class with a class variable company = "Infosys" and a class method to display it.
+
+# Q3
+
+# Create a MathTools class with a static method that prints the square of 9.
+
+# Q4
+
+# Create a Vehicle class with:
+
+# Instance variable: model
+# Instance method: show_model()
+# Q5
+
+# Create a College class with:
+
+# Class variable: college_name
+# Class method: display_college()
+# Intermediate (6–10)
+# Q6
