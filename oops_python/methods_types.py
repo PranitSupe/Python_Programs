141acf1a8d6415e1a1df025c26887b08866d0915
Pranit Supe <pranitsupe@gmail.com>
2026-07-18 21:54:56 +0530

    added python program  methods types

--- oops_python/methods_types.py
+++ oops_python/methods_types.py
@@ -15,6 +15,17 @@ s1.show_name()
 
 # Q2.Create an Employee class with a class variable company = "Infosys" and a class method to display it.
 
+
+class Employee:
+    company = "Infosys"
+
+    @classmethod
+    def show_name(cls):
+        print("company name is:",cls.company)
+
+e1=Employee()
+e1.show_name()
+
 # Q3.Create a MathTools class with a static method that prints the square of 9.
 
 # Q4.Create a Vehicle class with:
