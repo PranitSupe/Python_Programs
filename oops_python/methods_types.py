2e50c9d188296d1543349c441527a3bd4187bbae
Pranit Supe <pranitsupe@gmail.com>
2026-08-09 19:51:42 +0530

    added python program  methods types

--- oops_python/methods_types.py
+++ oops_python/methods_types.py
@@ -285,3 +285,11 @@ s1=ShoppingCart("mobile")
 s1.show_cart_item()
 ShoppingCart.show_store_name()
 print("Delivery charges:", s1.delivery_charges(1500))
+
+# Q13
+
+# Create a University class that demonstrates all three methods:
+
+# Instance method → display student details
+# Class method → display university name
+# Static method → display admission helpline number
